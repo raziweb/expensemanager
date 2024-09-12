338e9bef858a294c3fada5758909109bdc3561c4
Razi Shahid <mailrazi786@gmail.com>
2024-09-12 12:50:39 +0530

    Change order of date in fetched data

--- src/main/java/com/finance/expensemanager/repository/TransactionRepository.java
+++ src/main/java/com/finance/expensemanager/repository/TransactionRepository.java
@@ -12,12 +12,12 @@ import com.finance.expensemanager.model.entity.Transaction;
 public interface TransactionRepository extends JpaRepository<Transaction, Long>  {
 	List<Transaction> findByUserId(int userId);
 	
-	@Query(value = "SELECT * FROM transaction WHERE user = ?1 AND MONTH(date) = ?2 AND YEAR(date) = ?3 ORDER BY date", nativeQuery = true)
+	@Query(value = "SELECT * FROM transaction WHERE user = ?1 AND MONTH(date) = ?2 AND YEAR(date) = ?3 ORDER BY date DESC", nativeQuery = true)
 	List<Transaction> getCurrentMonthTransactions(int userId, int month, int year);
 	
 	@Query(value = "SELECT * FROM transaction WHERE user = ?1 AND date BETWEEN ?2 AND ?3 ORDER BY date", nativeQuery = true)
 	List<Transaction> getTransactionBetweenDates(int userId, String fromDate, String toDate);
 	
-	@Query(value = "SELECT * FROM transaction WHERE user = ?1 AND YEAR(date) = ?2 AND MONTH(date) = ?3 ORDER BY date", nativeQuery = true)
+	@Query(value = "SELECT * FROM transaction WHERE user = ?1 AND YEAR(date) = ?2 AND MONTH(date) = ?3 ORDER BY date DESC", nativeQuery = true)
 	List<Transaction> getTransactionsForGivenMonth(int userId, int year, int month);
 }
